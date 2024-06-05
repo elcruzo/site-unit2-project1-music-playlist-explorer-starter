7e9b80937294b4dc263403dbf61be5ba0399d6d2
Caleb Adekoya <caleb123@meta.com>
2024-06-05 00:27:34 -0700

    Created the wireframe for the music player

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,1 +1,17 @@
-
+document.addEventListener("DOMContentLoaded", function() { 
+    const likeButtons = document.getElementById("likeButton"); 
+    const likeCount = document.getElementById("likeCount"); 
+    
+    let count = 0; 
+    likeButton.addEventListener("click", function() { 
+        if (likeButton.classList.contains("liked")) { 
+            count--; 
+            likeButton.classList.remove("liked"); 
+        } 
+        else { 
+            count++; 
+            likeButton.classList.add("liked");
+        } 
+        likeCount.textContent = count;
+    });
+});
