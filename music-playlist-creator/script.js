dee56e1feb0ca6348423da5db25039ef626fa4eb
Caleb Adekoya <caleb123@meta.com>
2024-06-05 14:24:16 -0700

    Dynamic rendering

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -1,17 +1,49 @@
-document.addEventListener("DOMContentLoaded", function() { 
-    const likeButtons = document.getElementById("likeButton"); 
-    const likeCount = document.getElementById("likeCount"); 
-    
-    let count = 0; 
-    likeButton.addEventListener("click", function() { 
-        if (likeButton.classList.contains("liked")) { 
-            count--; 
-            likeButton.classList.remove("liked"); 
-        } 
-        else { 
-            count++; 
-            likeButton.classList.add("liked");
-        } 
-        likeCount.textContent = count;
-    });
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//     const likeButtons = document.getElementById("likeButton");
+//     const likeCount = document.getElementById("likeCount");
+
+//     let count = 0;
+//     likeButton.addEventListener("click", function() {
+//         if (likeButton.classList.contains("liked")) {
+//             count--;
+//             likeButton.classList.remove("liked");
+//         }
+//         else {
+//             count++;
+//             likeButton.classList.add("liked");
+//         }
+//         likeCount.textContent = count;
+//     });
+// });
+
+
+
+
+
+let myPlaylistData = data
+let playlists = document.getElementById('card-parent')
+const myPlaylistCards = myPlaylistData['playlists'].map((item) => {
+    console.log(item)
+    return (
+
+        `
+        <div class="card">
+            <div class="card-img">
+                <img src="${item['playlist_art']}" alt="" class="card-img">
+            </div>
+
+            <div>
+                <h3>${item['playlist_name']}</h3>
+                <p>${item['playlist_creator']}</p>
+
+                <div class="like-section">
+                    <i id="likeButton" class="fa-regular fa-heart"></i>
+                    <span id="likeButton">${item['likeCount']}</span>
+                </div>
+            </div>
+        </div>
+        `
+    )
+}).join('')
+
+playlists.innerHTML = myPlaylistCards
