db596d5a0d5c8b10488f9be8465bdce653effeb2
Caleb Adekoya <caleb123@meta.com>
2024-06-06 09:28:02 -0700

    Modal overlay updates

--- music-playlist-creator/script.js
+++ music-playlist-creator/script.js
@@ -16,6 +16,7 @@
 //     });
 // });
 
+
 function openModal(festival) {
     document.getElementById('festivalName').style.display = "block";
 }
@@ -24,52 +25,64 @@ function closeModal(festival) {
     document.getElementById('festivalName').style.display = "none";
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+    const myPlaylistData = data
+    const playlists = document.getElementById('card-parent')
+    const myPlaylistCards = myPlaylistData['playlists'].map((item) => {
+        console.log(item)
+        return (
+            `
+            <div class="card" onclick='openModal(${JSON.stringify(item)})'>
+                <div class="card-img">
+                    <img src="${item['playlist_art']}" alt="" class="img-card">
+                </div>
 
-let myPlaylistData = data
-let playlists = document.getElementById('card-parent')
-const myPlaylistCards = myPlaylistData['playlists'].map((item) => {
-    console.log(item)
-    return (
+                <div class="text-section">
+                    <h3>${item['playlist_name']}</h3>
+                    <p>${item['playlist_creator']}</p>
 
-        `
-        <div class="card">
-            <div class="card-img">
-                <img src="${item['playlist_art']}" alt="" class="img-card">
-            </div>
-
-            <div class="text-section">
-                <h3>${item['playlist_name']}</h3>
-                <p>${item['playlist_creator']}</p>
-
-                <div class="like-section">
-                    <i id="likeButton" class="fa-regular fa-heart"></i>
-                    <span id="likeButton">${item['likeCount']}</span>
+                    <div class="like-section">
+                        <i id="likeButton" class="fa-regular fa-heart"></i>
+                        <span id="likeButton">${item['likeCount']}</span>
+                    </div>
                 </div>
             </div>
-        </div>
-        `
-    )
-}).join('')
+            `
+        )
+    }).join('')
 
-playlists.innerHTML = myPlaylistCards
+    playlists.innerHTML = myPlaylistCards
 
-var modal = document.getElementById("festivalModal");
-var span = document.getElementsByClassName("close")[0];
+    const modal = document.getElementById("festivalModal");
+    const span = document.getElementsByClassName("close")[0];
 
-function openModal(festival) {
-   document.getElementById('festivalName').innerText = festival.name;
-   document.getElementById('festivalImage').src = festival.imageUrl;
-   document.getElementById('festivalDates').innerText = `Dates: ${festival.dates}`;
-   document.getElementById('festivalLocation').innerText = `Location: ${festival.location}`;
-   document.getElementById('artistLineup').innerHTML = `<strong>Lineup:</strong> ${festival.lineup.join(', ')}`;
-   modal.style.display = "block";
-}
+    window.openModal = function(playlist) {
+        document.getElementById('playlistImage').src = playlist.playlist_art;
+        document.getElementById('playlistName').innerText = playlist.playlist_name;
+        document.getElementById('playlistCreator').innerText = `By: ${playlist.playlist_creator}`;
 
-span.onclick = function() {
-   modal.style.display = "none";
-}
-window.onclick = function(event) {
-   if (event.target == modal) {
-      modal.style.display = "none";
-   }
-}
+        const songList = document.getElementById('songList');
+        songList.innerHTML = '<h3>Song List:</h3>';
+        playlist.songs.forEach(song => {
+            const songItem = document.createElement('div');
+            songItem.innerHTML =
+            `
+            <img src="${song.cover_art}" alt="Cover Art" style="width:50px; vertical-align: middle;">
+            <span>${song.title} - ${song.artist} (${song.duration})</span>
+            `
+            ;
+            songList.appendChild(songItem);
+        });
+        modal.style.display = "block";
+    };
+
+    span.onclick = function() {
+        modal.style.display = "none";
+    };
+
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+});
