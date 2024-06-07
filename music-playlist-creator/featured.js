08e6ba1d962bb015bfad17bc7eb8af10b668777b
Caleb Adekoya <caleb123@meta.com>
2024-06-07 14:49:32 -0700

    Fixed the featured playlist bug

--- music-playlist-creator/featured.js
+++ music-playlist-creator/featured.js
@@ -2,24 +2,29 @@ document.addEventListener("DOMContentLoaded", () => {
     const playlistData = data.playlists;
     const randomPlaylist = playlistData[Math.floor(Math.random() * playlistData.length)];
 
-    const featuredImg = document.querySelector(".featured-img");
+    const featuredImg = document.querySelector(".featured-img-left");
     const featuredName = document.getElementById("featured-name");
+    const featuredCreator = document.getElementById("featured-creator");
     const songList = document.getElementById("song-list");
 
     featuredImg.src = randomPlaylist.playlist_art;
     featuredName.textContent = randomPlaylist.playlist_name;
+    featuredCreator.textContent = randomPlaylist.playlist_creator;
 
     randomPlaylist.songs.forEach(song => {
         const songItem = document.createElement("li");
-        songItem.classList.add("eachsong");
+        songItem.classList.add("modal-each-song");
+
         songItem.innerHTML = `
             <img src="${song.cover_art}" alt="${song.title}" class="modal-each-song-img">
-            <div class="modal-each-song-desc">
-                <h3>${song.title}</h3>
-                <p><em>${song.album}</em></p>
-            </div>
-            <div class="modal-song-time">
-                <h3>${song.duration}</h3>
+            <div class="modal-song-parent-div">
+                <div class="modal-each-song-desc">
+                    <h3>${song.title}</h3>
+                    <p><em>${song.album}</em></p>
+                </div>
+                <div class="modal-song-time">
+                    <h3>${song.duration}</h3>
+                </div>
             </div>
         `;
         songList.appendChild(songItem);
